2145ad760bc7fccb217462c34853facc25e7c6cd
ponyspy <desade4me@gmail.com>
2016-02-17 13:32:05 +0300

    watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,46 @@
-var del = require('del');
-var runSequence = require('run-sequence');
+var del = require('del'),
+	 	colour = require('colour'),
+	 	runSequence = require('run-sequence');
 
-var gulp = require('gulp');
-var	juice = require('gulp-juice');
-var	jade = require('gulp-jade');
-var	stylus = require('gulp-stylus');
+var gulp = require('gulp'),
+		juice = require('gulp-juice'),
+		jade = require('gulp-jade'),
+	 	plumber = require('gulp-plumber'),
+		stylus = require('gulp-stylus');
+
+
+// Loggers Block
+
+var error_logger = function(error) {
+	console.log([
+		'',
+		'---------- ERROR MESSAGE START ----------'.bold.red.inverse,
+		'',
+		(error.name.red + ' in ' + error.plugin.yellow),
+		'',
+		error.message,
+		'----------- ERROR MESSAGE END -----------'.bold.red.inverse,
+		''
+	].join('\n'));
+}
+
+var watch_logger = function(event) {
+	console.log('File ' + event.path.green + ' was ' + event.type.yellow + ', running tasks...');
+}
+
+// Tasks Block
+
 
 gulp.task('stylus', function(){
 	return gulp.src('src/**/*.styl')
+		.pipe(plumber(error_logger))
 		.pipe(stylus())
 		.pipe(gulp.dest('src'));
 });
 
 gulp.task('jade', function() {
 	return gulp.src('src/mails/**/*.jade')
+		.pipe(plumber(error_logger))
 		.pipe(jade({pretty: true}))
 		.pipe(juice())
 		.pipe(gulp.dest('build'));
@@ -31,6 +58,12 @@ gulp.task('clean:before', function(callback) {
 gulp.task('clean:after', function(callback) {
 	return del('src/**/*.css', callback);
 });
+
+gulp.task('watch', function() {
+	gulp.watch(['src/**', '!src', '!src/*.css'], ['default']).on('change', watch_logger);
+});
+
+
 
 gulp.task('default', function(callback) {
 	runSequence('clean:before', ['stylus', 'images'], 'jade', 'clean:after', callback);
